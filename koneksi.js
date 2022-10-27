0565ce58f240b58afead718586d5ef1c1025d2f7
donjarot <yzdjarot@gmail.com>
2022-10-28 01:47:43 +0700

    menambahkan file koneksi database mysql

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+
+// Membuat koneksi database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    databas: 'dbrestapi'
+})
+
+conn.connect((error) => {
+    if (error) throw error
+    console.log('Koneksi ke Database berhasil');
+})
+
+module.exports = conn
