359c998fd58df82e648a820f5851921d963aec17
donjarot <yzdjarot@gmail.com>
2022-10-28 15:37:38 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -36,3 +36,52 @@ exports.registrasi = (req, res) => {
         }
     })
 }
+
+// Controller untuk login
+exports.login = (req, res) => {
+    const post = {
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+
+    const sql = `SELECT * FROM user WHERE email = '${post.email}' AND password = '${post.password}'`
+    connection.query(sql, (error, rows) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (rows.length == 1) {
+                const token = jwt.sign({ rows }, config.secret, {
+                    expiresIn: 1440
+                })
+                id_user = rows[0].id
+
+                const data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                const sqlAksesToken = `INSERT INTO akses_token (id_user, access_token, ip_address) VALUES ('${data.id_user}', '${data.access_token}', '${data.ip_address}')`
+
+                connection.query(sqlAksesToken, data, (error, rows) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        res.json({
+                            success: true,
+                            message: 'Token JWT tergenerate',
+                            token: token,
+                            currUser: data.id_user
+                        })
+                    }
+                })
+            } else {
+                res.json({
+                    'Error': true,
+                    'Message': 'Email atau password salah'
+                })
+            }
+        }
+    })
+
+}
